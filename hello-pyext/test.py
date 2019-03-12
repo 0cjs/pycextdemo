e8f9b81d3eeb3f1d20d395987c9e1cac6b8c6869
Curt J. Sampson <cjs@cynic.net>
2019-03-12 18:10:45 +0900

    hello-pyext/test.py: Extract an assert_equal function, return failure code
    ctest now relies on just the process exit code of the test to
    determine if it passed or failed.

--- hello-pyext/test.py
+++ hello-pyext/test.py
@@ -1,10 +1,17 @@
+import sys
 import phello
 
+def assert_equal(expected, actual, message):
+    if expected == actual:
+        return
+    print('    test:', message)
+    print('expected:', expected)
+    print('  actual:', actual)
+    print('FAIL')
+    sys.exit(1)
+
 def runtests():
-    if phello.seven() == 7:
-        print('OK')
-    else:
-        print('FAIL')
+    assert_equal(7, phello.seven(), 'seven()')
 
 if __name__ == '__main__':
     runtests()
